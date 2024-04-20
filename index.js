98012b6dbacc6b942b8dba5e2404c913f92ce4c9
Delta StudentKush <nkush995@gmail.com>
2024-04-20 23:19:46 +0530

    full functional with data base

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express=require('express');
 const app=express();
 const path=require("path");
 const methodOverride=require('method-override');
+const { v4: uuidv4 } = require("uuid");
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}));
@@ -126,6 +127,77 @@ app.get ('/user/:id/edit',(req,res)=>{
 
   
   }); 
+ 
+
+
+// new data route
+
+app.get("/user/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/user/new", (req, res) => {
+  let { username, email, password } = req.body;
+  let id = uuidv4();
+  //Query to Insert New User
+  let q = `INSERT INTO college (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      console.log("added new user");
+      res.redirect("/user");
+    });
+  } catch (err) {
+    res.send("some error occurred");
+  }
+});
+
+app.get("/user/:id/delete", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM college WHERE id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      res.render("delete.ejs", { user });
+    });
+  } catch (err) {
+    res.send("some error with DB");
+  }
+});
+
+app.delete("/user/:id/", (req, res) => {
+  let { id } = req.params;
+  let { password } = req.body;
+  let q = `SELECT * FROM college WHERE id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+
+      if (user.password != password) {
+        res.send("WRONG Password entered!");
+      } else {
+        let q2 = `DELETE FROM college WHERE id='${id}'`; //Query to Delete
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          else {
+            console.log(result);
+            console.log("deleted!");
+            res.redirect("/user");
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.send("some error with DB");
+  }
+});
+
+
 
 
  
